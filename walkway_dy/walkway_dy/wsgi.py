d1edd3ac2487a2af390eafce0abe4e1c1332ec2d
youngDaLee <ldy0956@gmail.com>
2021-03-05 18:24:19 +0900

    이다영:프로젝트생성

---
+++ walkway_dy/walkway_dy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for walkway_dy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'walkway_dy.settings')
+
+application = get_wsgi_application()
